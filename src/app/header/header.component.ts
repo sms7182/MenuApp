a1610e03c0b0582f0d840e38164535a2f8d39e78
mojtaba safavi <sms7182@gmail.com>
2019-12-13 18:51:25 +0330

    add fetch recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import {Store} from '@ngrx/store'
 
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions'
+import * as RecipeActions from '../recipes/store/recipe.actions'
 
 @Component({
     selector:'app-header',
@@ -32,7 +33,7 @@ export class HeaderComponent implements OnInit,OnDestroy{
     }
     onFetchData(){
        this.storageService.fetchRecipes().subscribe();
-     
+        this.store.dispatch(new RecipeActions.FetchRecipes());
     }
     onLogout(){
        this.store.dispatch(new AuthActions.Logout());
